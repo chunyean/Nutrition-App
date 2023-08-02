6639deba244ad325f2992e766a1f78a3f707bffa
chunyean <chunyeanfoong@gmail.com>
2023-08-02 13:15:59 +0800

    Update StandardRecipe.jsx

--- nutrition-app/src/components/StandardRecipe.jsx
+++ nutrition-app/src/components/StandardRecipe.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const StandardRecipe = () => {
-    return (
-        <div>
-            
-        </div>
+  const [recipe, setRecipe] = useState("");
+  const [mainIngr, setMainIngr] = useState("");
+  const [numIngr, setNumIngr] = useState();
+  const [cuisine, setCuisine] = useState("");
+  const [mealType, setMealType] = useState("");
+
+  const getData = async () => {
+    const res = await fetch(
+      import.meta.env.VITE_RECIPE +
+        mainIngr +
+        import.meta.env.VITE_RECIPE_APIKEY +
+        numIngr +
+        "&cuisineType=" +
+        cuisine +
+        "&mealType=" +
+        mealType +
+        "&imageSize=REGULAR"
     );
+    const data = await res.json();
+    setRecipe(data);
+  };
+
+  const handleMainIngr = () => {
+    setMainIngr(e.target.value)
+  }
+
+  const handleNumIngr = () => {
+    setNumIngr(e.target.value)
+  }
+
+  const handleCuisine = () => {
+    setCuisine(e.target.value)
+  }
+
+  const handleMealType = () => {
+    setMealType(e.target.value)
+  }
+
+  return <div className="container-md lg xl xxl">
+    <div className="row">
+        <h3>You can search any recipe by fill up the part at below</h3>
+        <input type="text"></input>
+    </div>
+  </div>;
 };
 
 export default StandardRecipe;
